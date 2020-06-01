454ca007bfcd61d4dac6d10761c8159717acf1e4
Hossprog <hossprog@gmail.com>
2020-06-01 19:58:59 +0200

    safe

--- index.js
+++ index.js
@@ -5,34 +5,6 @@ const express = require('express')
 const bodyparser = require('body-parser')
 const app = express()
 const rou = express.Router()
-const mongoose = require('mongoose')
 
-
-const url  = "mongodb://localhost:27017"
-mongoose.connect(url, { useNewUrlParser: true ,useUnifiedTopology: true })
-const db = mongoose.connection
-
-app.use(bodyparser.urlencoded({ extended: true }))
-app.use(express.static('public'))
-app.use(bodyparser.json())
-app.use('/api',require('./routes/api'))
-
-
-rou.get('/api/ninja',(req,res,next)=>{
-db.collection('ninjas').aggregate([
-{
-$geoNear: {
-near: { type: "Point", coordinates: [-80,25] },
-distanceField: "dist.calculated",
-maxDistance: 100000,
-query: { category: "Parks" },
-includeLocs: "dist.location",
-spherical: true
-}
-}
-], (err,docs)=>{
-res.json(docs); 
-})
-}); 
 
 app.listen(process.env.port || 3020)
